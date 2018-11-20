b94e5e2a7dc5e1530aa287ac4716b27a11c3b3d5
Ben Cornelis <bencornelis02@gmail.com>
2018-11-20 07:31:15 -0600

    get timer form with multiple timers working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import Timer from './components/2.6';
+import TimerApp from './components/2.6';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Timer />
+        <TimerApp />
       </div>
     );
   }
--- src/components/2.6.js
+++ src/components/2.6.js
@@ -10,10 +10,18 @@ const Circle = styled.div`
   };
   background: ${({ fadeOut }) => fadeOut ? 'white' : 'orange'};
   color: white;
-  font-size: ${({ downBeat, size}) => downBeat ? 0.5 * size : 0.5 * size}px;
+  font-size: ${({ downBeat, size}) => downBeat ? 0.5 * size : 0.55 * size}px;
   text-align: center;
   vertical-align: middle;
   line-height: ${({ downBeat, size }) => downBeat ? size : 1.1 * size}px;
+`;
+
+const TimerWrapper = styled.div`
+  width: ${({ size }) => 1.1 * size}px;
+  height: ${({ size }) => 1.1 * size}px;
+  display:flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 class Timer extends Component {
@@ -28,7 +36,7 @@ class Timer extends Component {
 
     this.state = {
       time,
-      isSmall: true,
+      downBeat: true,
       fadeOut: false,
     };
 
@@ -68,21 +76,112 @@ class Timer extends Component {
 
   render() {
     const { downBeat, time, fadeOut } = this.state;
-    if (time < 0) {
+    if (time <= 0) {
       return null;
     }
 
     return (
-      <Circle
-        downBeat={downBeat}
-        size={this.size}
-        beatPeriod={this.beatPeriod}
-        fadeOut={fadeOut}
-        >
-        {time}
-      </Circle>
-    )
+      <TimerWrapper size={this.size}>
+        <Circle
+          downBeat={downBeat}
+          size={this.size}
+          beatPeriod={this.beatPeriod}
+          fadeOut={fadeOut}
+          >
+          {time}
+        </Circle>
+      </TimerWrapper>
+    );
   }
 }
 
-export default Timer;
+const Form = styled.form`
+`;
+const Input = styled.input`
+`;
+const Button = styled.button`
+`;
+
+class TimerForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
+  }
+
+  handleChange = e => {
+    this.setState({ value: e.target.value });
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit(parseInt(this.state.value));
+    this.setState({ value: '' });
+  }
+
+  render() {
+    return (
+      <Form>
+        <Input onChange={this.handleChange} value={this.state.value} />
+        <Button type='submit' onClick={this.handleSubmit}>Start boiling</Button>
+      </Form>
+    );
+  }
+}
+
+const Container = styled.div`
+  height: 200px;
+  width: 600px;
+  border: 1px solid grey;
+`;
+
+const _id = (() => {
+  let __id = 0;
+  return () => {
+    __id += 1;
+    return __id;
+  }
+})();
+
+class TimerApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      timers: []
+    };
+  }
+
+  addTimer = time => {
+    this.setState(({ timers }) => {
+      const id = _id();
+      timers = [...timers, { id, time }];
+      console.log(timers)
+      return { timers };
+    });
+  }
+
+  removeTimer = id => {
+    this.setState(({ timers }) => {
+      timers = timers.filter(timer => timer.id !== id);
+      return { timers };
+    });
+  }
+
+  render() {
+    const { timers } = this.state;
+    return (
+      <Container>
+        <TimerForm onSubmit={this.addTimer} />
+        {timers.map(({ id, time }) =>
+          <Timer
+            key={id}
+            time={time}
+            size={50}
+            onComplete={() => this.removeTimer(id)}
+            />
+        )}
+      </Container>
+    );
+  }
+}
+
+export default TimerApp;
